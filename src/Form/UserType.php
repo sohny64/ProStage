b11d1bb17a6d6f075b7be8785ab184b985554f7a
Miguel Viegas <mvdfigueired@iutbayonne.univ-pau.fr>
2021-03-30 12:07:28 +0200

    Mise en place de CRUD

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -15,7 +17,15 @@ class UserType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('email')
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                'type'=>PasswordType::class,
+                'invalid_message' => 'Les mots de passe saisis ne correspondent pas',
+                'options'=>['attr' => ['class' => 'password-field']],
+                'required'=> true,
+                'first_options'=>['label'=>'Mot de passe'],
+                'second_options'=>['label'=>'Répéter la saisie du mot de passe'],
+
+            ])
 
         ;
     }
--- src/Security/FormulaireLoginAuthenticator.php
+++ src/Security/FormulaireLoginAuthenticator.php
@@ -86,7 +86,7 @@ class FormulaireLoginAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('accueil'));
+        return new RedirectResponse($this->urlGenerator->generate('prostages_accueil'));
     }
 
     protected function getLoginUrl()
