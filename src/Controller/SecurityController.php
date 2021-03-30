ca7a47d2f39867a84558acf67a3445e67f86b87d
Miguel Viegas <mvdfigueired@iutbayonne.univ-pau.fr>
2021-03-30 12:30:43 +0200

    installation de easyadmin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,7 +31,7 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-
+      
     }
 
     /**
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,4 +148,9 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getNom();
+    }
 }
